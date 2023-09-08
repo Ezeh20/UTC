c9252058a6cdf13d7f3d43fb9cfb0c811df6239a
Ezeh20 <ranter1570@gmail.com>
2023-09-08 23:22:32 +0100

    abeg na

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const DAY = document.getElementById("day")
 
 function getUtcTime() {
     const utcTime = new Date();
-    const day = utcTime.getDay() + 1
-    utcTime.setHours(utcTime.getHours() + 1);;
-    const watTime = utcTime.toUTCString();
+    const utc = utcTime.getTime()
     const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    UTC.textContent = `UTC Time :  ${watTime}`
-    DAY.textContent = `Day of the week  : ${daysOfTheWeek[utcTime.getDay()]}, the ${day}th day of the week.`
+    UTC.textContent = `UTC Time :  ${utc}`
+    DAY.textContent = `Day of the week  : ${daysOfTheWeek[utcTime.getDay()]}`
 }
 setInterval(getUtcTime, 1000);
