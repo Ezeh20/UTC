14f03d284a3bd6659b23582fb497c9a8d1384540
Ezeh20 <ranter1570@gmail.com>
2023-09-08 19:35:01 +0100

    ready to push

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const DAY = document.getElementById("day")
 
 const date = new Date()
 const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+DAY.textContent = ` ${daysOfTheWeek[date.getDay()]}`
+
+function getUtcTime() {
+    const utcTime = new Date();
+    utcTime.setMinutes(utcTime.getMinutes() - utcTime.getTimezoneOffset())
+    const time = utcTime.toISOString().slice(11, -1)
+    UTC.textContent = `${time}`
+}
+getUtcTime();
+setInterval(getUtcTime, 100);
 
 
-DAY.textContent = `${daysOfTheWeek[date.getDay()]}`;
-UTC.textContent = date.getTime();
