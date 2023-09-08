12d734d697f5da0104e5823d7051e5bad0286963
Ezeh20 <ranter1570@gmail.com>
2023-09-08 21:58:03 +0100

    ..

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const UTC = document.getElementById("utc")
 const DAY = document.getElementById("day")
 
-const date = new Date()
-const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-DAY.textContent = `Day : ${daysOfTheWeek[date.getDay()]}`
+
 
 function getUtcTime() {
     const utcTime = new Date();
-    const time = utcTime.getTime();
-    UTC.textContent = `UTC Time : ${time}`
+    const day = utcTime.getDay() + 1
+    utcTime.setHours(utcTime.getHours() + 1);;
+    const watTime = utcTime.toUTCString();
+    const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    UTC.textContent = `UTC Time :  ${watTime}`
+    DAY.textContent = `Today is  : ${daysOfTheWeek[utcTime.getDay()]}, the ${day}th day of the week.`
 }
-setInterval(getUtcTime, 1);
-
-
+setInterval(getUtcTime, 1000);
